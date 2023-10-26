3dfcc517bed4f6fd3d72965566c025bdf4053040
Aviciy <gh0004@ukr.net>
2023-10-26 18:55:47 +0300

    Doc str

--- Shared/connector.py
+++ Shared/connector.py
@@ -14,24 +14,30 @@ class Connector(object):
 
     @abstractmethod
     def check_connection(self):
+        '''Checking the relevance of the connection'''
         raise NotImplementedError
 
     @abstractmethod
     def get_server_time(self):
+        '''returning the server time'''
         raise NotImplementedError
 
     @abstractmethod
     def get_exchange_info(self):
+        '''Returns the exchange data'''
         raise NotImplementedError
 
     @abstractmethod
     def get_ticker(self, symbol):
+        '''Returns information about the Symbol'''
         raise NotImplementedError
 
     @abstractmethod
     def get_book(self, symbol):
+        '''Returns order book'''
         raise NotImplementedError
 
     @abstractmethod
     def get_balances(self):
+        '''Returns balance information'''
         raise NotImplementedError
