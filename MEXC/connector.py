84cbedcb603e5771802d8c3ea76c79fcd9023dca
Aviciy <gh0004@ukr.net>
2023-10-27 08:17:11 +0300

    Doc str

--- MEXC/connector.py
+++ MEXC/connector.py
@@ -1,32 +1,42 @@
-import logging
-import logging.config
+from abc import ABCMeta, abstractmethod
 
-from Shared.connector import Connector
+class Connector(object):
+    __metadata__ = ABCMeta
 
+    def __init__(self):
+        pass
 
-class MEXCConnector(Connector):
-    def __init__(self, settings):
-        super().__init__()
-        self.__settings = settings
-        self.__connector_name = 'MEXC'
-
+    @abstractmethod
     def get_name(self):
+        '''Returns the name of the exchange'''
         raise NotImplementedError
 
+    @abstractmethod
     def check_connection(self):
+        '''Checking the relevance of the connection'''
         raise NotImplementedError
 
+    @abstractmethod
     def get_server_time(self):
+        '''returning the server time'''
         raise NotImplementedError
 
+    @abstractmethod
     def get_exchange_info(self):
+        '''Returns the exchange data'''
         raise NotImplementedError
 
+    @abstractmethod
     def get_ticker(self, symbol):
+        '''Returns information about the Symbol'''
         raise NotImplementedError
 
+    @abstractmethod
     def get_book(self, symbol):
+        '''Returns order book'''
         raise NotImplementedError
 
+    @abstractmethod
     def get_balances(self):
+        '''Returns balance information'''
         raise NotImplementedError
--- Shared/logger.py
+++ Shared/logger.py
@@ -1,6 +1,14 @@
+import logging
+
 class Logger:
     def __init__(self, log_file):
-        pass
+        logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger()
 
     def log(self, message: str):
-        pass
+        self.logger.debug(message)
+
+if __name__ == "__main__":
+    logger = Logger("my_log_file.log")
+    logger.log("Це повідомлення для лог-файлу (debug)")
+
