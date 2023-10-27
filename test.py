8cca5a2609f342c726c9915f3a4833a813864fca
Aviciy <gh0004@ukr.net>
2023-10-27 18:41:14 +0300

    4

--- test.py
+++ test.py
@@ -12,15 +12,3 @@ with open('MEXC_settings.json', 'r') as file:
 
 logger.debug(settings)
 connector = MEXCConnector(logger, settings)
-
-
-def on_market_top(instance_name, coin, top):
-    print('{}-{} top  Bid: {:f}({:f}) Ask: {:f}({:f})'.
-          format(instance_name,
-                 coin,
-                 top['bid'],
-                 top['bidSize'],
-                 top['ask'],
-                 top['askSize'],
-                 )
-          )
